7c3461153c2fff351e75f372d9f4aafec90d4fbc
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-07-30 21:32:03 +0300

    added get and post endpoints in task servlet

---
+++ src/main/java/org/fasttrackit/config/ObjectMapperConfiguration.java
@@ -0,0 +1,20 @@
+package org.fasttrackit.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ObjectMapperConfiguration {
+
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static ObjectMapper getObjectMapper() {
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+        }
+
+        return objectMapper;
+    }
+
+}
